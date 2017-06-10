dae4bcdcd1a1f89f627e5e6ad73f7ee1ba6f8197
Vincent Wasteels <vincent.wasteels@gmail.com>
2017-06-10 16:04:14 +0200

    copy from python

---
+++ example.js
@@ -0,0 +1,4 @@
+const motors = require("./").motors
+const maxSpeed = require("./").maxSpeed
+
+motors.motor2.setSpeed(480)
---
+++ index.js
@@ -0,0 +1,56 @@
+const wpi = require("wiring-pi")
+
+const maxSpeed = 480
+const ioInitialized = false
+
+function ioInit() {
+	if(ioInitialized) return
+	
+	wpi.wiringPiSetupGpio()
+	wpi.pinMode(12, wpi.GPIO.PWM_OUTPUT)
+	wpi.pinMode(13, wpi.GPIO.PWM_OUTPUT)
+
+	wpi.pwmSetMode(wpi.GPIO.PWM_MODE_MS)
+	wpi.pwmSetRange(maxSpeed)
+	wpi.pwmSetClock(2)
+
+	wpi.pinMode(5, wpi.GPIO.OUTPUT)
+	wpi.pinMode(6, wpi.GPIO.OUTPUT)
+
+	ioInitialized = true
+}
+
+class Motor {
+	constructor(pwmPin, dirPin) {
+		this.pwmPin = pwmPin
+		this.dirPin = dirPin
+	}
+	setSpeed(speed) {
+		var dirValue
+		if(speed < 0) {
+			speed = -speed
+			dirValue = 1
+		} else {
+			dirValue = 0
+		}
+		ioInit()
+		wpi.digitalWrite(this.dirPin, dirValue)
+		wpi.pwmWrite(this.pwmPin, speed)
+	}
+}
+
+class Motors {
+	constructor() {
+		this.motor1 = new Motor(12, 5)
+		this.motor2 = new Motor(13, 6)
+	}
+	setSpeeds(m1Speed, m2Speed) {
+		this.motor1.setSpeed(m1Speed)
+		this.motor2.setSpeed(m2Speed)
+	}
+}
+
+module.exports = {
+	motors: new Motors(),
+	maxSpeed: maxSpeed
+}
