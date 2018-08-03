9852763b57bbdfb6f6fb6d3efc4caf02455e9eaf
Aaron Silber <aaron@burlingtonbytes.com>
2018-08-03 19:41:32 -0400

    alternate speed

--- example.js
+++ example.js
@@ -9,5 +9,15 @@ let motor2 = motors.motor2;
 console.log('motor1',motor1);
 console.log('motor2',motor2);
 
-motor1.setSpeed(128);
-motor2.setSpeed(128);
+let currentSpeed = 128;
+console.log('wait for first setSpeed...');
+setInterval(() => {
+  if ( currentSpeed == 128 ) {
+    currentSpeed = 0;
+  } else {
+    currentSpeed = 128;
+  }
+  console.log('setting new currentSpeed: ', currentSpeed);
+  motor1.setSpeed(currentSpeed);
+  motor2.setSpeed(currentSpeed);
+}, 2000);
