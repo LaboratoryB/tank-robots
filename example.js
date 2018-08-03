2c9d6884704a68a6bd5a2f5eb204efd15d3996a4
Aaron Silber <aaron@burlingtonbytes.com>
2018-08-03 19:53:33 -0400

    only run motors up to 175/255

--- example.js
+++ example.js
@@ -9,13 +9,14 @@ let motor2 = motors.motor2;
 console.log('motor1',motor1);
 console.log('motor2',motor2);
 
-let currentSpeed = 128;
-console.log('wait for first setSpeed...');
+let maxSafeSpeed = 175;
+let currentSpeed = 0;
+console.log('wait for first setSpeed command...');
 setInterval(() => {
-  if ( currentSpeed == 128 ) {
+  if ( currentSpeed == maxSafeSpeed ) {
     currentSpeed = 0;
   } else {
-    currentSpeed = 128;
+    currentSpeed = maxSafeSpeed;
   }
   console.log('setting new currentSpeed: ', currentSpeed);
   motor1.setSpeed(currentSpeed);
