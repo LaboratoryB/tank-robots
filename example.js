a82e9287f5de257ea0b6642bea47e2ccf5654bc5
Aaron Silber <aaron@burlingtonbytes.com>
2018-08-03 19:30:31 -0400

    logging

--- example.js
+++ example.js
@@ -1,5 +1,7 @@
-const motors = require("./").motors
-const maxSpeed = require("./").maxSpeed
+const motors = require("./index.js").motors;
+const maxSpeed = require("./index.js").maxSpeed;
+
+console.log('motors',motors);
 
 motors.motor2.setSpeed(480);
 motors.motor1.setSpeed(480);
