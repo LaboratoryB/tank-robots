417894418ad8f3dde4f31bbdb0ea24bdfc963f22
Aaron Silber <aaron@burlingtonbytes.com>
2018-08-03 19:38:51 -0400

    control both motors in example.js

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
 const base     = require("./index.js");
 const motors   = new base.motors();
-const maxSpeed = require("./index.js").maxSpeed;
+const maxSpeed = base.maxSpeed;
 
 console.log('motors',motors);
 
+let motor1 = motors.motor1;
 let motor2 = motors.motor2;
+console.log('motor1',motor1);
 console.log('motor2',motor2);
 
-motor2.setSpeed(480);
-//motors.motor1.setSpeed(480);
+motor1.setSpeed(128);
+motor2.setSpeed(128);
