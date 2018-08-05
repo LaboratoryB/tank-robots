7e0e4cce5fd85e544d07e31cbe7395ecffa51054
Greg Schoppe <gschoppe@gmail.com>
2018-08-05 15:15:55 -0400

    use callback to time commands

--- example.js
+++ example.js
@@ -9,10 +9,11 @@ let i = 0;
 // Note: stop will force an early termination of commands, if necessary. in most cases it can be replaced by a "skip turn"
 //       fire is not yet implemented
 let commands = ['forwards', 'stop', 'backwards', 'left', 'right', 'fire'];
-setInterval(() => {
-  tank.parseCommand( commands[c] );
+let doCommand = function() {
+  tank.parseCommand( commands[c], 1, () => { doCommand(); } );
   i = ( i + 1 ) % commands.length;
-}, 3000);
+};
+doCommand();
 
 // TODO: add support for registering an enemy fire callback
 //       add support for registering a tank-disabled callback
