316d64a7cdf996749d6f77ea76505619dc9f96d3
Greg Schoppe <gschoppe@gmail.com>
2018-08-05 20:19:59 -0400

    use request.js to provide commands

--- example.js
+++ example.js
@@ -1,19 +1,24 @@
 const tc_lib   = require("./index.js");
+const request  = require("request");
 const os       = require("os");
 const tank     = new tc_lib.Tank();
 const hostname = os.hostname();
 const endpoint = 'https://vtbots.com/wp-admin/admin-ajax.php';
 
-let i = 0;
-// Begin test of all functions
-// Note: stop will force an early termination of commands, if necessary. in most cases it can be replaced by a "skip turn"
-//       fire is not yet implemented
-let commands = ['forwards', 'stop', 'backwards', 'left', 'right', 'fire'];
-let doCommand = function() {
-  tank.parseCommand( commands[c], 1, () => { doCommand(); } );
-  i = ( i + 1 ) % commands.length;
-};
-doCommand();
+let request_command = function() {
+  let uri = endpoint + '?action=vtbots_tank_get_command&hostname=' + hostname;
+  request(uri, { json: true }, (err, res, body) => {
+    let command = 'wait';
+    let resolution = 1;
+    if (err) {
+      console.log(err);
+    } else {
+      command = body.command;
+      resolution = body.resolution;
+    }
+    tank.parseCommand( command, resolution, () => { request_command(); } );
+  });
+}
 
 // TODO: add support for registering an enemy fire callback
 //       add support for registering a tank-disabled callback
