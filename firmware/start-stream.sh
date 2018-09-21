19309849e77d17ba3bacc988583032c029ca8e99
Aaron Silber <aaron@burlingtonbytes.com>
2018-09-21 18:13:27 -0400

    cameras do not need 180 rotation - mounted correct side up

--- firmware/start-stream.sh
+++ firmware/start-stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # pass one arg that is the RTMP endpoint
-raspivid -rot 180 -o - -t 0 -w 1280 -h 720 -fps 24 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 $1
+raspivid -rot 0 -o - -t 0 -w 1280 -h 720 -fps 24 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 $1
