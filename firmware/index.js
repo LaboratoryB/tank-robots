9a342c46849e66de342e4b7ce8ac0070bb2fd454
Greg Schoppe <gschoppe@gmail.com>
2018-09-21 19:19:18 -0400

    add minimum delay between network requests

--- firmware/index.js
+++ firmware/index.js
@@ -1,15 +1,16 @@
-const tc_lib   = require("./tank-control.js");
-const request  = require("request");
-const os       = require("os");
-const hostname = os.hostname();
+const tc_lib     = require("./tank-control.js");
+const request    = require("request");
+const os         = require("os");
+const hostname   = os.hostname();
 
-const Config   = require('./config.js');
-let config     = new Config('./private/config.json');
-let tank       = new tc_lib.Tank();
+const Config     = require('./config.js');
+let config       = new Config('./private/config.json');
+let tank         = new tc_lib.Tank();
+let requestDelay = 500;
 
 let register_hit = function() {
-  let endpoint = config.getConfig('command_endpoint');
-  let apikey   = config.getConfig('api_key');
+  let endpoint   = config.getConfig('command_endpoint');
+  let apikey     = config.getConfig('api_key');
   let uri = endpoint + '?action=vtbots_tank_register_hit&hostname=' + hostname + "&apikey=" + apikey;
   request(uri, { json: true }, (err, res, body) => {});
 }
@@ -29,7 +30,9 @@ let request_command = function() {
       resolution = body.resolution;
       console.log( "Response from server: got command ", command, " and resolution ", resolution );
     }
-    tank.parseCommand( command, resolution, () => { request_command(); } );
+    tank.parseCommand( command, resolution, () => { 
+      setTimeout( ()=>{ request_command(); }, requestDelay );
+    } );
   });
 }
 request_command();
